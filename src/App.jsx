90527cc8e8355af280315522b2e3775e2979c526
hrmrakib <hrmrakibs@gmail.com>
2024-11-10 11:32:37 +0600

    basic route set-up

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div className='bg-[#0c0e15]'>App</div>;
-};
-
-export default App;
---
+++ src/layouts/Root.jsx
@@ -0,0 +1,10 @@
+const Root = ({ children }) => {
+  return (
+    <div>
+      <h2>navbar</h2>
+      {children}
+    </div>
+  );
+};
+
+export default Root;
