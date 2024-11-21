737562926bde8ba350b9bebcf18199560b5078f5
hrmrakib <hrmrakibs@gmail.com>
2024-11-21 14:28:45 +0600

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,34 +1,35 @@
 import React from "react";
+import Container from "../../container/Container";
 
 const Footer = () => {
   return (
-    <footer class='relative mt-24'>
-      <div class='w-[1200px] mx-auto pb-9'>
-        <div class='grid grid-cols-1 lg:grid-cols-6 items-start gap-4'>
-          <div class=''>
+    <footer className='relative mt-24'>
+      <Container className='pb-9'>
+        <div className='grid grid-cols-1 lg:grid-cols-6 items-start gap-4'>
+          <div className=''>
             <a href='#'>
-              <img src='./assets/icons/logo.png' alt='logo' />
+              <img src='/assets/icons/logo.png' alt='logo' />
             </a>
-            <div class='flex items-center gap-2 mt-5'>
+            <div className='flex items-center gap-2 mt-5'>
               <a href='#'>
-                <img src='./assets/icons/instragram.png' alt='instragram' />
+                <img src='/assets/icons/instragram.png' alt='instragram' />
               </a>
               <a href='#'>
-                <img src='./assets/icons/facebook.png' alt='facebook' />
+                <img src='/assets/icons/facebook.png' alt='facebook' />
               </a>
               <a href='#'>
-                <img src='./assets/icons/twitter.png' alt='twitter' />
+                <img src='/assets/icons/twitter.png' alt='twitter' />
               </a>
               <a href='#'>
-                <img src='./assets/icons/youtybe.png' alt='youtybe' />
+                <img src='/assets/icons/youtybe.png' alt='youtybe' />
               </a>
             </div>
           </div>
 
-          <div class='flex flex-col items-center justify-center'>
-            <h2 class='text-base font-bold text-white'>Buy Crypto</h2>
+          <div className='flex flex-col items-center justify-center'>
+            <h2 className='text-base font-bold text-white'>Buy Crypto</h2>
 
-            <ul class='flex flex-col gap-2 mt-2.5 *:text-[#B6B6B6]'>
+            <ul className='flex flex-col gap-2 mt-2.5 *:text-[#B6B6B6]'>
               <li>
                 <a href='#'>Exchanges</a>
               </li>
@@ -44,10 +45,10 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div class='flex flex-col items-center justify-center border-x border-x-[#1e222a] -mt-10 py-8'>
-            <h2 class='text-base font-bold text-white'>About Us</h2>
+          <div className='flex flex-col items-center justify-center border-x border-x-[#1e222a] -mt-10 py-8'>
+            <h2 className='text-base font-bold text-white'>About Us</h2>
 
-            <ul class='flex flex-col gap-2 mt-2.5 *:text-[#B6B6B6]'>
+            <ul className='flex flex-col gap-2 mt-2.5 *:text-[#B6B6B6]'>
               <li>
                 <a href='#'>About</a>
               </li>
@@ -66,12 +67,12 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div class='flex flex-col items-center justify-center border-r border-x-[#1e222a] -mt-12 py-[54px]'>
-            <h2 class='w-full text-base font-bold text-white pl-7'>
+          <div className='flex flex-col items-center justify-center border-r border-x-[#1e222a] -mt-12 py-[54px]'>
+            <h2 className='w-full text-base font-bold text-white pl-7'>
               Help Center
             </h2>
 
-            <ul class='flex flex-col gap-2 mt-2.5 *:text-[#B6B6B6]'>
+            <ul className='flex flex-col gap-2 mt-2.5 *:text-[#B6B6B6]'>
               <li>
                 <a href='#'>Contact US</a>
               </li>
@@ -87,20 +88,24 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div class='lg:col-span-2'>
-            <h2 class='text-base font-bold text-white'>Newsletter</h2>
-            <p class='max-w-[250px] text-[#B6B6B6] leading-5 mt-2'>
+          <div className='lg:col-span-2'>
+            <h2 className='text-base font-bold text-white'>Newsletter</h2>
+            <p className='max-w-[250px] text-[#B6B6B6] leading-5 mt-2'>
               Never miss anything crypto when you’re on go
             </p>
 
-            <div class='max-w-[355px] mt-4 flex items-center gap-2 border border-purple-950 rounded-xl p-2'>
+            <div className='max-w-[355px] mt-4 flex items-center gap-2 border border-purple-950 rounded-xl p-2'>
               <input
-                class='bg-transparent text-white w-[70%] outline-none border-none'
+                className='bg-transparent text-white w-[70%] outline-none border-none'
                 type='text'
                 placeholder='Your Email'
               />
-              <button class='bg-gradient-to-l from-[#FFBB8C] via-[#FF77BA] to-[#A869FF] flex items-center gap-2 text-white px-2.5 py-2 rounded-md'>
-                <img class='w-4 h-4' src='./assets/icons/bell.svg' alt='bell' />
+              <button className='bg-gradient-to-l from-[#FFBB8C] via-[#FF77BA] to-[#A869FF] flex items-center gap-2 text-white px-2.5 py-2 rounded-md'>
+                <img
+                  className='w-4 h-4'
+                  src='/assets/icons/bell.svg'
+                  alt='bell'
+                />
                 Subscribe
               </button>
             </div>
@@ -108,25 +113,25 @@ const Footer = () => {
         </div>
 
         {/* copyright  */}
-        <div class='w-[70%] h-1 mx-auto border-b border-b-[#1e222a]'></div>
-        <p class='pt-10 pb-6 text-sm text-[#B6B6B6] text-center'>
+        <div className='w-[70%] h-1 mx-auto border-b border-b-[#1e222a]'></div>
+        <p className='pt-10 pb-6 text-sm text-[#B6B6B6] text-center'>
           Copyright © 2024. Moneychain . All rights reserved.
         </p>
-      </div>
+      </Container>
 
       <img
-        class='absolute w-[227px] h-[232px] -left-12 bottom-0'
-        src='./assets/images/bitcoin-2.png'
+        className='absolute w-[227px] h-[232px] -left-12 bottom-0'
+        src='/assets/images/bitcoin-2.png'
         alt=''
       />
       <img
-        class='absolute left-1/2 transform -translate-x-1/2 bottom-0'
-        src='./assets/images/footer-blur.png'
+        className='absolute left-1/2 transform -translate-x-1/2 bottom-0'
+        src='/assets/images/footer-blur.png'
         alt=''
       />
       <img
-        class='absolute right-0 -bottom-0'
-        src='./assets/images/footer-boll.png'
+        className='absolute right-0 -bottom-0'
+        src='/assets/images/footer-boll.png'
         alt=''
       />
     </footer>
--- src/container/Container.jsx
+++ src/container/Container.jsx
@@ -1,5 +1,9 @@
-const Container = ({ children }) => {
-  return <div className='w-[94%] xl:w-[1200px] mx-auto'>{children}</div>;
+const Container = ({ children, className }) => {
+  return (
+    <div className={`w-[94%] xl:w-[1200px] mx-auto ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
